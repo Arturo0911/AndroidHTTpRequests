c309d9c05484031e5afa660159599dc1f6c71b42
Arturon <anegreiross@outlook.com>
2021-01-03 16:54:15 -0500

    2

---
+++ app/src/main/java/com/example/usersprocesstesting/SecondTest.java
@@ -0,0 +1,73 @@
+package com.example.usersprocesstesting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.VoiceInteractor;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+public class SecondTest extends AppCompatActivity {
+
+    private Button buttonClick;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_test);
+
+        buttonClick = findViewById(R.id.buttonClick);
+
+        buttonClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RequestQueue requestQueue = Volley.newRequestQueue(SecondTest.this);
+                String URL = "192.168.100.255:5000/api";
+
+                /**
+                 * Creates a new request.
+                 *
+                 * @param method the HTTP method to use
+                 * @param url URL to fetch the JSON from
+                 * @param jsonRequest A {@link JSONObject} to post with the request. Null indicates no
+                 *     parameters will be posted along with request.
+                 * @param listener Listener to receive the JSON response
+                 * @param errorListener Error listener, or null to ignore errors.
+                 */
+                JsonObjectRequest objectRequest = new JsonObjectRequest(
+                        Request.Method.GET,
+                        URL,
+                        null,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                Log.e("Rest response", response.toString());
+                                Toast.makeText(SecondTest.this, "Rest response: "+response.toString(), Toast.LENGTH_SHORT).show();
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Log.e("Rest response", error.toString());
+                                Toast.makeText(SecondTest.this, "Rest response: "+error.toString(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                );
+                requestQueue.add(objectRequest);
+            }
+        });
+
+
+    }
+}
